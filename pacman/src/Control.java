cd37af18bea163d7d8a781519c309e6f819ff46d
Oscar Lam <oscarl9173665@gmail.com>
2022-06-03 09:57:04 -0400

    Movement added

--- pacman/src/Control.java
+++ pacman/src/Control.java
@@ -4,37 +4,46 @@ import javax.swing.*;
 
 public class Control implements KeyListener {
 	Pacman gui;
-	final int BOARD_SIZE = gui.BOARD_SIZE;
-//	key = gui.e.getKeycode();
 	
+	public Control (Pacman in){
+		gui = in;
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == 87 || e.getKeyCode() == 38) {
-			System.exit(0);
-			for (int row = 0; row < BOARD_SIZE; row++) {
-				for (int col = 0; col < BOARD_SIZE; col++) {
-					if (gui.map[row][col] == 1) {
-						gui.map[row][col] = 0;
-						gui.map[row + 1][col] = 1;
-						break;
-					}
-				}
-			}
-			
-			gui.mapRead();
+		if (e.getKeyCode() == 87) { // up
+			gui.map[gui.playerR][gui.playerC] = 0;
+			gui.playerR--;
+			gui.map[gui.playerR][gui.playerC] = 1;
 		} 
+		else if (e.getKeyCode() == 83) { // down
+			gui.map[gui.playerR][gui.playerC] = 0;
+			gui.playerR++;
+			gui.map[gui.playerR][gui.playerC] = 1;
+		} 
+		else if (e.getKeyCode() == 68) { // right
+			gui.map[gui.playerR][gui.playerC] = 0;
+			gui.playerC++;
+			gui.map[gui.playerR][gui.playerC] = 1;
+		} 
+		else if (e.getKeyCode() == 65) { // left
+			gui.map[gui.playerR][gui.playerC] = 0;
+			gui.playerC--;
+			gui.map[gui.playerR][gui.playerC] = 1;
+		} 
+		gui.mapRead();
+		gui.printMap(); 
 	}	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 }
--- pacman/src/Pacman.java
+++ pacman/src/Pacman.java
@@ -1,9 +1,10 @@
-
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -13,9 +14,10 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class Pacman extends JFrame {
-	Control control;
+	Control control = new Control(this);
 	
 	final int BOARD_SIZE = 20;
+	int playerC = 0, playerR = 0;
 	JPanel panel = new JPanel();
 	int[][] map = {
 			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
@@ -41,10 +43,10 @@ public class Pacman extends JFrame {
 			};
 	JLabel[][] board = new JLabel[BOARD_SIZE][BOARD_SIZE];
 	int name = 0;
-	ImageIcon blackTile = new ImageIcon("images/blackTile.png");
-	ImageIcon brownTile = new ImageIcon("images/brownTile.png");
-	ImageIcon whiteTile = new ImageIcon("images/whiteTile.png");
-	ImageIcon pacman = new ImageIcon("images/pacAnt.png");
+	ImageIcon blackTile = new ImageIcon("blackTile.png");
+	ImageIcon brownTile = new ImageIcon("brownTile.png");
+	ImageIcon whiteTile = new ImageIcon("whiteTile.png");
+	ImageIcon pacman = new ImageIcon("pacAnt.png");
 	
 	private JPanel contentPane;
 
@@ -79,22 +81,39 @@ public class Pacman extends JFrame {
 		
 		GridLayout layout1 = new GridLayout(BOARD_SIZE, BOARD_SIZE, 1, 1);
 		panel.setLayout(layout1);
-
 		for (int row = 0; row < BOARD_SIZE; row++) {
 			for (int col = 0; col < BOARD_SIZE; col++) {
-				name++;
-
-				board[row][col] = new JLabel(Integer.toString(name));
-				board[row][col].setText("");
-
+				board[row][col] = new JLabel();
+				if(map[row][col]==0) {
+					board[row][col].setIcon(blackTile);
+				}
+				else if(map[row][col]==1) {
+					board[row][col].setIcon(brownTile);
+					playerR = row;
+					playerC = col;
+				}
 				panel.add(board[row][col]);
 			}
 		}
 		
-		mapRead();
+		this.add( panel, BorderLayout.CENTER  );
 		
-		getContentPane().add(panel);
 		setVisible(true);
+		addKeyListener(control);
+	}
+	
+	public void printMap()
+	{  // print map to console for debugging
+		for (int i = 0; i < board.length; i++ ) 
+		{
+			for (int j = 0; j < board[0].length; j++ ) 
+			{
+				System.out.print("  "+map[i][j]);                    
+
+			} 
+			System.out.println();
+		}
+		System.out.println();
 	}
 	
 	public void mapRead() {
@@ -114,4 +133,5 @@ public class Pacman extends JFrame {
 			}
 		}
 	}
+
 }
