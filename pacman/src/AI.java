b94f6223d11f64473348e6ffcdbc464c46ea2abb
Oscar Lam <oscarl9173665@gmail.com>
2022-06-09 09:16:26 -0400

    Add files via upload

--- pacman/src/AI.java
+++ pacman/src/AI.java
@@ -5,22 +5,28 @@ import javax.swing.ImageIcon;
 
 public class AI implements ActionListener {
 	Pacman gui;
+	boolean redBeeFirst = true;
+	boolean greenBeeFirst = true;
+	boolean purpleBeeFirst = true;
+	
 
 	public AI (Pacman in){
 		gui = in;
 	}
 	
 	public void redBee() {
-		if(gui.redR-1>=0 && gui.map[gui.redR-1][gui.playerC] == 0) // up
+		if(gui.redR-1>=0 && gui.map[gui.redR-1][gui.redC] == 0) // up
 		{
-			gui.map[gui.redR][gui.playerC] = 0;
+			gui.map[gui.redR][gui.redC] = 0;
 			gui.redR--;
-			gui.map[gui.redR][gui.playerC] = 1;
+			gui.map[gui.redR][gui.redC] = 1;
 		}
+		if(redBeeFirst == true) {
 		if(gui.redR == 10 && gui.redC == 9) {
 			gui.map[gui.redR][gui.redC] = 0;
 			gui.redC++;
 			gui.map[gui.redC][gui.redC] = 4;
+		}
 		}
 		gui.mapRead();
 		gui.printMap();
